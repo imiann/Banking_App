0a9b5e15589e40999c8a7b503686c64c498fa075
Ian McFarland <ian.mcfarland8@gmail.com>
2024-12-28 20:35:43 +0100

    Add and implement complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,39 @@
+import RightSidebar from '@/components/ui/RightSidebar';
+import HeaderBox from '@/components/ui/HeaderBox'
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstName: 'Ian', lastName: 'McFarland', email: 'ian.mcfarland8@gmail.com'};
+
+
   return (
-    <div>Home</div>
+    <section className ="home">
+        <div className="home-content">
+            <header className="home-header">
+                <HeaderBox 
+                    type="greeting"
+                    title="Welcome"
+                    user={loggedIn?.firstName || 'Guest'}
+                    subtext="Access and manage your account and transaction efficiently."
+                />
+
+                <TotalBalanceBox 
+                    accounts={[]}
+                    totalBanks={1}
+                    totalCurrentBalance={1250.36}
+                />
+            </header>
+
+            RECENT TRANSACTIONS
+        </div>
+
+        <RightSidebar 
+            user={loggedIn}
+            transactions={[]}
+            banks={[{ currentBalance: 123.50}, {currentBalance: 500.12}]}
+        />
+    </section>
   )
 }
 
